2c96cbda4521cbde9ddf95053390761c5be1aae3
Søren <Sren@DESKTOP-3DFGV3A>
2017-08-23 09:37:03 +0200

    Add project files.

---
+++ AuctionHouse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace AuctionHouse
+{
+    class Program
+    {
+        StreamReader sr;
+        StreamWriter sw;   
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.RunServer();
+        }
+
+        void RunServer()
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(ip, 11000);
+            server.Start();
+
+            while (true)
+            {
+                Socket client = server.AcceptSocket();
+                Console.WriteLine($"Client found with ip: {client.RemoteEndPoint}");
+            }
+        }
+    }
+}
