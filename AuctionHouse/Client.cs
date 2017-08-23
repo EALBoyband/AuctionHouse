631e35dc68d883413c0e1a2079d98ca5b8cb2f43
UrbanSkurk <UrbanSkurk@HELL>
2017-08-23 10:59:24 +0200

    vf

--- AuctionHouse/Client.cs
+++ AuctionHouse/Client.cs
@@ -5,17 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
-
+using System.IO;
 namespace AuctionHouse
 {
     class Client
     {
         private Socket s;
         NetworkStream stream;
+        StreamReader sr;
+        StreamReader sw;
+        
 
         public Client(Socket s)
         {
             this.s = s;
+            stream = new NetworkStream(s);
+            sr = new StreamReader(stream);
+            sw = new StreamReader(stream);
             
         }
     }
